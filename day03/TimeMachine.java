64d3b386af5d7293c4c3bf94e2a61b697f978876
Layla <layla.biologie@gmail.com>
2022-01-18 14:37:11 -0500

    fourth point

--- day03/TimeMachine.java
+++ day03/TimeMachine.java
@@ -5,6 +5,7 @@ public class TimeMachine {
     public static void main(String[] args) {
         System.out.println("first point");
         System.out.println("second point");
+        System.out.println("third point");
     }
 
 
